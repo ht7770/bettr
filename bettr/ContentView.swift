3ac130d39dd02500104e9bdc570aaee6c7c98a13
ht7770 <58001312+ht7770@users.noreply.github.com>
2021-12-31 15:51:04 +0000

    Initial Commit

---
+++ bettr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bettr
+//
+//  Created by Henry Thomas on 31/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bettr/bettrApp.swift
@@ -0,0 +1,17 @@
+//
+//  bettrApp.swift
+//  bettr
+//
+//  Created by Henry Thomas on 31/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct bettrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
