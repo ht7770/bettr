8a4b496231a9cc3a381ff62c55e2f722b726ae05
ht7770 <58001312+ht7770@users.noreply.github.com>
2022-09-29 19:13:33 +0100

    Added the first part of the AddTask popover view, which contains the text boxes for title and description, need a deeper understanding of TextEditor to move forwards however

--- bettr/Views/Main Views/AddTaskPopover.swift
+++ bettr/Views/Main Views/AddTaskPopover.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct AddTaskPopover: View {
+    
+    @State private var taskTitle: String = ""
+    @State private var taskDesc: String = ""
+    
     var body: some View {
-        Text("add task popover test!")
+        VStack {
+            VStack(alignment: .leading){
+                Text("Task title:")
+                    .font(.callout)
+                    .bold()
+                
+                TextField("Enter title", text: $taskTitle)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            .padding()
+            VStack(alignment: .leading){
+                Text("Task Description:")
+                    .font(.callout)
+                    .bold()
+                
+                TextEditor(text: $taskDesc)
+                    .background(Color.gray)
+                    .frame(minWidth: 0, minHeight: 0, maxHeight: 200)
+                    .cornerRadius(10)
+                
+    
+                
+            }
+            .padding()
+        }
+        
     }
 }
 
