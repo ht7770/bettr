b9ad3901bd2ff7920094a9300477f3cf20892b48
ht7770 <58001312+ht7770@users.noreply.github.com>
2022-10-03 11:22:57 +0100

    Added support for the task model being saved when the app is moved to background / likely to be terminated

--- bettr/bettrApp.swift
+++ bettr/bettrApp.swift
@@ -12,12 +12,19 @@ import Foundation
 @main
 struct bettrApp: App {
     
+    @Environment(\.scenePhase) private var scenePhase
+    
     @StateObject private var taskModelData = TaskModelData()
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(taskModelData)
+        }
+        .onChange(of: scenePhase){ phase in
+            if phase == .background{
+                taskModelData.saveTaskModel()
+            }
         }
     }
 }
