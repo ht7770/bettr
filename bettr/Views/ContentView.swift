1d53760715fbafdd88800f6f9e014050b68457ff
ht7770 <58001312+ht7770@users.noreply.github.com>
2022-10-18 12:14:11 +0100

    Added a tab bar to the application, this allows the user to switch from the tasks view to the new settings view

--- bettr/Views/ContentView.swift
+++ bettr/Views/ContentView.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var tabSelection: Tab = .tasks
+    
+    enum Tab{
+        case tasks
+        case settings
+        
+    }
+    
+    init(){
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+        UITabBar.appearance().barTintColor = .black
+    }
+    
     var body: some View {
-        TaskList()
+        TabView(selection: $tabSelection){
+            TaskList()
+                .tabItem{
+                    Label("Tasks", systemImage: "list.bullet")
+                }
+                .tag(Tab.tasks)
+            
+            
+            SettingsView()
+                .tabItem{
+                    Label("Settings", systemImage: "gear")
+                }
+                .tag(Tab.settings)
+        }
+        
+        .accentColor(Color("tabFontColour"))
+        
+
             
         }
     
---
+++ bettr/Views/Main Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  bettr
+//
+//  Created by Henry Thomas on 18/10/2022.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings Page")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
