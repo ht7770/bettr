2b39c3eda8d70e4c5e546cb30c22031428fb5ac3
ht7770 <58001312+ht7770@users.noreply.github.com>
2022-10-18 13:46:17 +0100

    Added a form to the settings view to start fleshing it out, and beginning to add support for a user profile, which can be tied to the task list.

--- bettr/Views/Main Views/SettingsView.swift
+++ bettr/Views/Main Views/SettingsView.swift
@@ -8,10 +8,40 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State private var name: String = ""
+    
     var body: some View {
-        Text("Settings Page")
+        NavigationView{
+            Form{
+                Section(header: Text("PROFILE")) {
+                    TextField("Name", text: $name)
+                }
+                
+                Section(header: Text("ABOUT")){
+                    HStack{
+                        Text("Version")
+                        Spacer()
+                        Text("1.0")
+                    }
+                    
+                }
+                
+                Section{
+                    Button(action: {
+                        print("Erase all tasks and reload model")
+                    }, label: {
+                        Text("Erase all Content and Settings")
+                            .foregroundColor(Color.red)
+                    })
+                    }
+                }
+            .navigationTitle("Settings")
+           
+                
+            }
+        .background(backgroundColour())
+        }
     }
-}
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
